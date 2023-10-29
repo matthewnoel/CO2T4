7434548f16ee3d116475bdd36be2e0c85b9ff5c0
Matthew Noel <31493274+matthewnoel@users.noreply.github.com>
2023-10-29 16:24:40 -0700

    Generate third-party licenses

--- src/components/Warning.svelte
+++ src/components/Warning.svelte
@@ -14,7 +14,7 @@
     <h1>Warning!</h1>
     <h2>Read Before Continuing</h2>
     <p>This tool is an approximate unofficial guide for the <strong>Box Breathing</strong> protocol described in <a href="https://doi.org/10.1016/j.xcrm.2022.100895" target="_blank" rel="noreferrer">Brief Structured Respiration Practices Enhance Mood and Reduce Physiological Arousal</a></p>
-    <p>This tool is open source. The source code <a href="https://github.com/matthewnoel/CO2T4" target="_blank" rel="noreferrer">is available here</a>.</p>
+    <p>This tool is open source. The source code <a href="https://github.com/matthewnoel/CO2T4" target="_blank" rel="noreferrer">is available here</a>. Third-party licenses <a href="https://github.com/matthewnoel/CO2T4/blob/main/third-party-licenses.txt" target="_blank" rel="noreferrer">are available here</a>.</p>
     <p><strong class="important">This tool is intended for educational use only.</strong></p>
     <p><strong class="important">This tool is not affiliated with the authors of the aforementioned paper.</strong></p>
     <p><strong class="important">This tool does not provide medical advice. Do not use this tool or any breathing exercise without consulting a licensed physician.</strong></p>
